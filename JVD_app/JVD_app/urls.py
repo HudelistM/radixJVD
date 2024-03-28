c1a93e0e25933b68330e091dce6bcbc6b65bae0f
MMokii <matej.molnarr@gmail.com>
2024-03-28 02:09:13 +0100

    commit

--- JVD_app/JVD_app/urls.py
+++ JVD_app/JVD_app/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import include,path
 from scheduler import views
 from django_browser_reload.urls import urlpatterns as reload_urls
-from scheduler.views import schedule_view,api_schedule_data,download_schedule
+from scheduler.views import schedule_view,api_schedule_data,download_schedule, radnici
 
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path('admin/', admin.site.urls),
     path('', views.landingPage, name='landingPage'),  # Root URL
     path('accounts/', include('django.contrib.auth.urls')),
+    path('radnici/', radnici, name='radnici'),
+    path('add_employee/', views.add_employee, name='add_employee'),
     path('register/', views.register, name='register'),
     path('schedule/', schedule_view, name='schedule_view'),
     path('update_schedule/', views.update_schedule, name='update_schedule'),
--- JVD_app/scheduler/forms.py
+++ JVD_app/scheduler/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Employee
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,3 +9,8 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ['name', 'surname', 'role', 'group']
