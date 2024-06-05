11e437aafd6da6e7a5b27772e7c9ab000847c5ac
HudelistM <marin.hudelist@gmail.com>
2024-06-05 22:32:40 +0200

    Finally fixed gitignore

--- JVD_app/passenger_wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, '/home/jvp-djur/shihta.jvp-djurdjevac.hr/repositories/radixJVD/JVD_app')
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'JVD_app.settings'
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
