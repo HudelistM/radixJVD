16eb9346269288524aad082743091cdd8dcd244d
HudelistM <marin.hudelist@gmail.com>
2024-11-07 10:22:53 +0100

    chore: Update settings.py

--- JVD_app/JVD_app/settings.py
+++ JVD_app/JVD_app/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    # Add other static file directories here
+    os.path.join(BASE_DIR, 'static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
